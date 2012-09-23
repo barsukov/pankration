e346a92d8250a2a5c60e2ad0f2d051bee9d10201
barsukov <pro.barsukov@gmail.com>
2012-09-23 19:58:06 +0800

    test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pankration::Application.config.secret_token = '92181e96b31ee70e57347bf02e8f0f3c09c0d7a4789102f656682274b85d749d06dace8989c9f4f560e01caa29129110ad613107405935274187dfe0861112c8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Pankration::Application.config.session_store :cookie_store, key: '_pankration_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Pankration::Application.config.session_store :active_record_store
